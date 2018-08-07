65f0673437e12a01a687218ea054ece281c73d8e
Sarah Heckendorn <praisinghim@jcangus.com>
2018-08-07 09:48:13 -0600

    added bootstrap, still fine tuning

--- index.php
+++ index.php
@@ -6,39 +6,71 @@
 	<title>Oh Snap! Challenge</title>
 </head>
 <body>
-	<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-		<div class="navbar-nav ml-auto">
-			<a class="nav-item nav-link active" href="#">Home<span class="sr-only">(current)</span></a>
-			<a class="nav-item nav-link" href="#">Link</a>
-			<a class="nav-item nav-link" href="#">Dropdown</a>
+	<div>
+		<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+			<a class="navbar-brand" href="#">Welcome!</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Link</a>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Dropdown
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">Set</a>
+							<a class="dropdown-item" href="#">Phasers</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="#">Beep</a>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</div>
+	<div class="container-fluid px-4">
+		<div class="row py-3">
+			<div class="card col-md-3">
+				<h5 class="card-header">Riker</h5>
+				<div class="card-body">
+					<p class="card-text">Sensors indicate human life forms 30 meters below the planet's surface. Stellar flares are increasing in magnitude and frequency. Set course for Rhomboid Dronegar 006, warp seven.</p>
+				</div>
+			</div>
+			<div class="card col-md-3">
+				<h5 class="card-header">Featured</h5>
+				<div class="card-body">
+					<p class="card-text">Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators.</p>
+				</div>
+			</div>
+			<div class="card col-md-3">
+				<h5 class="card-header">Featured</h5>
+				<div class="card-body">
+					<p class="card-text">I have reset the sensors to scan for frequencies outside the usual range. By emitting harmonic vibrations to shatter the lattices. We will monitor and adjust the frequency of the resonators.</p>
+				</div>
+			</div>
+			<div class="card col-md-3">
+				<h5 class="card-header">Featured</h5>
+				<div class="card-body">
+					<p class="card-text">These are the voyages of the Starship Enterprise. Its continuing mission, to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no one has gone before.</p>
+				</div>
+			</div>
 		</div>
 	</div>
-	<div>
-		<div>
-			<p>Riker</p>
-			<p>Sensors indicate human life forms 30 meters below the planet's surface. Stellar flares are increasing in magnitude and frequency. Set course for Rhomboid Dronegar 006, warp seven. There's no evidence of an advanced communication network. Total guidance system failure, with less than 24 hours' reserve power. Shield effectiveness has been reduced 12 percent.</p>
-		</div>
-		<div>
-			<p>Ipsum</p>
-			<p>Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.</p>
-		</div>
-		<div>
-			<p>---</p>
-			<p>I have reset the sensors to scan for frequencies outside the usual range. By emitting harmonic vibrations to shatter the lattices. We will monitor and adjust the frequency of the resonators. He has this ability of instantly interpreting and extrapolating any verbal communication he hears. It may be due to the envelope over the structure, causing hydrogen-carbon helix patterns throughout.</p>
-		</div>
-		<div>
-			<p><3</p>
-			<p>These are the voyages of the Starship Enterprise. Its continuing mission, to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no one has gone before. We need to neutralize the homing signal. Each unit has total environmental control, gravity, temperature, atmosphere, light, in a protective field. Sensors show energy readings in your area.</p>
-		</div>
-	</div>
-	<div>
-		<div>
+	<div class="row">
+		<div class="col-md-4">
 			<p>Sensors indicate human life forms 30 meters below the planet's surface. Stellar flares are increasing in magnitude and frequency. Set course for Rhomboid Dronegar 006, warp seven. There's no evidence of an advanced communication network. Total guidance system failure, with less than 24 hours' reserve power. Shield effectiveness has been reduced 12 percent. We have covered the area in a spherical pattern which a ship without warp drive could cross in the given time. Resistance is futile.</p>
 		</div>
-		<div>
+		<div class="col-md-4">
 			<p>It indicates a synchronic distortion in the areas emanating triolic waves. The cerebellum, the cerebral cortex, the brain stem,  the entire nervous system has been depleted of electrochemical energy. Any device like that would produce high levels of triolic waves. These walls have undergone some kind of selective molecular polarization. I haven't determined if our phaser energy can generate a stable field. We could alter the photons with phase discriminators.</p>
 		</div>
-		<div>
+		<div class="col-md-4">
 			<p>Unidentified vessel travelling at sub warp speed, bearing 235.7. Fluctuations in energy readings from it, Captain. All transporters off. A strange set-up, but I'd say the graviton generator is depolarized. The dark colourings of the scrapes are the leavings of natural rubber, a type of non-conductive sole used by researchers experimenting with electricity. The molecules must have been partly de-phased by the anyon beam.</p>
 		</div>
 	</div>
